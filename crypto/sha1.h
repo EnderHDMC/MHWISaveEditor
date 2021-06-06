9744e5921f50506601ffddc0303b64391259d4a6
EnderHDMC <16001028+EnderHDMC@users.noreply.github.com>
2021-06-06 14:23:49 +0200

    Fixed possible overflow for x64

--- crypto/sha1.h
+++ crypto/sha1.h
@@ -7,7 +7,7 @@ static u8* GenerateHash(u8* save, int length, int offset)
 {
     u8* hash = (u8*)malloc(sizeof(u8) * 20);
     CryptoPP::SHA1 sha1;
-    sha1.CalculateDigest(hash, (save + offset), length - offset);
+    sha1.CalculateDigest(hash, save + offset, ((size_t)length) - offset);
     byteswap(hash, 20);
     return hash;
 }
