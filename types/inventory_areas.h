130bf245279fb8f4a3ed559b5081430262ec7f14
EnderHDMC <16001028+EnderHDMC@users.noreply.github.com>
2021-06-12 12:44:43 +0200

    Added the other inventory areas

--- types/inventory_areas.h
+++ types/inventory_areas.h
@@ -16,10 +16,15 @@ struct inventory_area {
   int localoffset;
   int count;
   item_type type;
+  bool storage;
   char* area;
 };
 
 const inventory_area areas[] = {
-  {offsetof(mhw_save_slot, item_pouch) + offsetof(mhw_item_pouch, items), COUNTOF(mhw_item_pouch::items), item_type::Item, "Item Pouch"},
-  {offsetof(mhw_save_slot, item_pouch) + offsetof(mhw_item_pouch, ammo), COUNTOF(mhw_item_pouch::ammo), item_type::Ammo, "Ammo Pouch"}
+  {offsetof(mhw_save_slot, item_pouch) + offsetof(mhw_item_pouch, items), COUNTOF(mhw_item_pouch::items), item_type::Item, false, "Item Pouch"},
+  {offsetof(mhw_save_slot, item_pouch) + offsetof(mhw_item_pouch, ammo), COUNTOF(mhw_item_pouch::ammo), item_type::Ammo, false , "Ammo Pouch"},
+  {offsetof(mhw_save_slot, storage) + offsetof(mhw_storage, items), COUNTOF(mhw_storage::items), item_type::Item, true, "Item Box" },
+  {offsetof(mhw_save_slot, storage) + offsetof(mhw_storage, ammo), COUNTOF(mhw_storage::ammo), item_type::Ammo, true, "Ammo Box"},
+  {offsetof(mhw_save_slot, storage) + offsetof(mhw_storage, materials), COUNTOF(mhw_storage::materials), item_type::Material, true, "Material Box"},
+  {offsetof(mhw_save_slot, storage) + offsetof(mhw_storage, decorations), COUNTOF(mhw_storage::decorations), item_type::Decoration, true, "Deco Box"}
 };
