d09d943768a4286bf90d695b943ee20847dfbde5
EnderHDMC <16001028+EnderHDMC@users.noreply.github.com>
2024-06-23 15:21:52 +0200

    Log registry read

--- utility/system/registry.h
+++ utility/system/registry.h
@@ -11,10 +11,16 @@ public:
     if (regSteam.childKeys().contains(member))
       value = regSteam.value(member);
 
+    qInfo() << "Registry Query:" << path << "Member:" << member;
+    qInfo() << "Registry Value:" << value.toString();
     return value;
   }
 
   static inline const QStringList Groups(const QString& path) {
-    return QSettings(path, QSettings::NativeFormat).childGroups();
+    QStringList groups = QSettings(path, QSettings::NativeFormat).childGroups();
+    qInfo() << "Registry Groups:" << path;
+    qInfo() << groups.join('\n');
+
+    return groups;
   }
 };
